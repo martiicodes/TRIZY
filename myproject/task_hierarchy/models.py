27e162ceaf7c6b646e6bb7c42c2511921655974c
Nenad Milcanovic <nenadmilcanovic@proton.me>
2024-08-12 13:27:18 +0200

    Delete myproject directory

--- myproject/task_hierarchy/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Task(models.Model):
-    name = models.CharField(max_length=200)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
-
-    def __str__(self):
-        return self.name
